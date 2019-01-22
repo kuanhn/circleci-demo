46af0238fb428ea629a707bc2d118be8ecc48cf4
HoangQuan <quan_hoang@irep.co.jp>
2019-01-22 16:44:41 +0900

    Remove Sonarqube shell

--- .circleci/analyze-sonarqube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mvn sonar:sonar \
-  -Dsonar.projectKey="${PROJECT_KEY}" \
-  -Dsonar.organization="${ORGANIZATION}" \
-  -Dsonar.host.url="${SONARQUBE_URL}"\
-  -Dsonar.login="${SONARQUBE_LOGIN}"
